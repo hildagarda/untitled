0a4d3c59e14b5f683bbe648c4d5b6d84836cf5f8
hildagarda <Sanbonzakura5@gmail.com>
2022-02-02 21:34:03 +0500

    create chess board

---
+++ src/Chess/Main.java
@@ -0,0 +1,16 @@
+package Chess;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board.arr1);
+        System.out.println(board.arr2);
+        board.getLines();
+        System.out.println(board.arr4);
+        System.out.println(board.arr3);
+
+
+
+    }
+
+}
